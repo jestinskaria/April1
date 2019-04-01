eb263b75dbf1fda7572595b94dfeced417fd1648
jestin skaria <jestinskaria15@gmail.com>
2019-04-01 17:00:32 -0400

    final commit of thed day

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 var http = require('http');
 
+var a = 5;
+var b =10;
+var c = a+b;
+
+function greet(){
+ var greet = " hi this is the last";
+ return greet;
+}
+
 http.createServer(function (req, res) {
   // add a HTTP header:
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('Hello World!');
+  res.write('Hello !');
+
+  res.write('Sum is '+c);
+
+  res.write(greet());
+  
   res.end();
 }).listen(8080);
